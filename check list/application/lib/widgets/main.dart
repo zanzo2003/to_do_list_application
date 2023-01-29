7106b0efcb41c60afb7a2f6b13db661fe2e58c5a
Shashwath Bhaskar <shashwathbhaskar@Shashwaths-MacBook-Pro.local>
2023-01-29 10:39:06 +0530

    Setting Up-mSetting up the development enviorment and arrangement of the code for easy readablity

---
+++ check list/application/lib/widgets/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../screens/home.dart';
+
+void main() {
+  return (runApp(const MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "ToDo App",
+      home: Home(),
+    );
+  }
+}
--- check list/application/test/widget_test.dart
+++ check list/application/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:application/main.dart';
+import 'package:application/widgets/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
